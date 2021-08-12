e603d905cc7f266f87d10405c43aca6a096fdf07
jacob <jacobharv00@gmail.com>
2021-08-11 18:19:30 -0600

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,26 +5,12 @@ class Post < ApplicationRecord
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :clickbait?
 
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i, 
-    /Secret/i, 
-    /Top \d/i, 
-    /Guess/i
-  ]
+  patterns = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
 
   def clickbait?
-    if CLICKBAIT_PATTERNS.none? {|pattern| pattern.match title}
+    if patterns.none? {|pattern| pattern.match title}
       errors.add(:title, "must be clickbait")
     end
   end
-
-  # Alternate shorter way
-
-  # def clickbait?
-  #   if title && !title.include?("Won't Believe")
-  #     errors.add(:title, "must be clickbait")
-  #   end
-  # end
-
 
 end
